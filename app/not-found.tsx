7f2e463bf42ee24701a6e48370218c1eb2d7e1f1
Abhishek Mishra <amishr1@stevens.edu>
2023-04-23 20:39:50 -0400

    footer

--- app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-const NotFound = () => {
-  return (
-    <div className="text-red-900 flex-1 text-center">
-      <div className="text-xl pt-20">Error 404 : Page Not Found</div>
-    </div>
-  );
-};
-export default NotFound;
