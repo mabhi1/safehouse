c1628aa7786371d0f8c13d337ba2f944a2317c8a
Abhishek Mishra <amishr1@stevens.edu>
2023-04-23 20:31:03 -0400

    footer

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+const NotFound = () => {
+  return (
+    <div className="text-red-900 flex-1 text-center">
+      <div className="text-xl pt-20">Error 404 : Page Not Found</div>
+    </div>
+  );
+};
+export default NotFound;
---
+++ app/passwords/page.tsx
@@ -0,0 +1,5 @@
+type Props = {};
+const Passwords = (props: Props) => {
+  return <div>Passwords</div>;
+};
+export default Passwords;
