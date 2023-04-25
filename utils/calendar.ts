5f09c2221c4ff351467f5e19271b01bd8c0eeb87
Abhishek Mishra <amishr1@stevens.edu>
2023-04-25 04:43:09 -0400

    calendar

--- utils/calendar.ts
+++ utils/calendar.ts
@@ -7,18 +7,18 @@ export const generateDate = (month = dayjs().month(), year = dayjs().year(), tas
 
   const arrayOfDate = [];
 
-  const checkEvents = (d: dayjs.Dayjs) => {
-    return (
-      task.filter((t: TaskType) => {
-        if (
-          new Date(t.from).getDate() === d.toDate().getDate() &&
-          new Date(t.from).getFullYear() == d.toDate().getFullYear() &&
-          new Date(t.from).getMonth() == d.toDate().getMonth()
-        )
-          return t;
-      }).length > 0
-    );
-  };
+  // const checkEvents = (d: dayjs.Dayjs) => {
+  //   return (
+  //     task.filter((t: TaskType) => {
+  //       if (
+  //         new Date(t.from).getDate() === d.toDate().getDate() &&
+  //         new Date(t.from).getFullYear() == d.toDate().getFullYear() &&
+  //         new Date(t.from).getMonth() == d.toDate().getMonth()
+  //       )
+  //         return t;
+  //     }).length > 0
+  //   );
+  // };
 
   // create prefix date
   for (let i = 0; i < firstDateOfMonth.day(); i++) {
@@ -26,14 +26,14 @@ export const generateDate = (month = dayjs().month(), year = dayjs().year(), tas
     arrayOfDate.push({
       currentMonth: false,
       date,
-      events: checkEvents(date),
+      events: false,
     });
   }
 
   // generate current date
   for (let i = firstDateOfMonth.date(); i <= lastDateOfMonth.date(); i++) {
     arrayOfDate.push({
-      events: checkEvents(firstDateOfMonth.date(i)),
+      events: false,
       currentMonth: true,
       date: firstDateOfMonth.date(i),
       today: firstDateOfMonth.date(i).toDate().toDateString() === dayjs().toDate().toDateString(),
@@ -44,7 +44,7 @@ export const generateDate = (month = dayjs().month(), year = dayjs().year(), tas
 
   for (let i = lastDateOfMonth.date() + 1; i <= lastDateOfMonth.date() + remaining; i++) {
     arrayOfDate.push({
-      events: checkEvents(firstDateOfMonth.date(i)),
+      events: false,
       currentMonth: false,
       date: lastDateOfMonth.date(i),
     });
