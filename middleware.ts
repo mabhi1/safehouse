6729bab3d28e5cd2495c4a9b53962e465c2a14cf
Abhishek Mishra <mabhishek0221@gmail.com>
2025-01-24 18:33:29 -0500

    feat: updated middleware for crypto api access

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/api/cron", "/api/passwords"]);
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/cron",
+  "/api/passwords",
+  "/api/crypto",
+]);
 
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
